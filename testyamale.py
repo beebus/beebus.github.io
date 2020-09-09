b9ccb0b13eb94fdcda19c6ded3217f5ccc96cc56
Michael Beebower <3438663+beebus@users.noreply.github.com>
2020-09-09 01:40:41 -0500

    Update testyamale.py
    Added docstrings

--- testyamale.py
+++ testyamale.py
@@ -1,11 +1,11 @@
-# Import Yamale and make a schema object:
-import yamale
-schema = yamale.make_schema('./schema.yaml')
+def testyamle():
+    """ Import the yamale library and make a schema object"""
+    import yamale
+    schema = yamale.make_schema('./schema.yaml')
 
-# Create a Data object
-data = yamale.make_data('./_pages/')
+    """ Create a Data object"""
+    data = yamale.make_data('./_pages/')
 
-# Validate data against the schema. Throws a ValueError if data is invalid.
-
-for datum in data:
-    yamale.validate(schema, datum)
+    """ Validate data against the schema. Throws a ValueError if data is invalid."""
+    for datum in data:
+        yamale.validate(schema, datum)
