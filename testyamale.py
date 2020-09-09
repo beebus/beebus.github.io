f2e664f0f1d5bb7d7b25e469b1455a5253b58125
Michael Beebower <3438663+beebus@users.noreply.github.com>
2020-09-09 01:45:48 -0500

    Update testyamale.py
    Correct the docstring formatting

--- testyamale.py
+++ testyamale.py
@@ -1,11 +1,11 @@
+""" 
+Import the yamale library and make a schema object 
+Create a Data object
+Validate data against the schema. Throws a ValueError if data is invalid.
+"""
 def testyamle():
-    """ Import the yamale library and make a schema object"""
     import yamale
     schema = yamale.make_schema('./schema.yaml')
-
-    """ Create a Data object"""
     data = yamale.make_data('./_pages/')
-
-    """ Validate data against the schema. Throws a ValueError if data is invalid."""
     for datum in data:
         yamale.validate(schema, datum)
